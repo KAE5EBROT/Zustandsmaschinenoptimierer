9ffd76d36ce4ac5791562983867fafb22aa3d9f7
Patrick <patrick.wenninger1@freenet.de>
2018-05-11 17:21:30 +0200

    scheiß git

--- lex.cpp
+++ lex.cpp
@@ -117,12 +117,23 @@ int	CParser::yyparse()
 
 	Zustandscodierung = optimize(high_priority, mean_priority, lowest_priority);
 
-	writeOutputFile();
-	//writeOutputFile(Zustandscodierung);
-	smtable::elementlist temp;//testdatei für Schäfers minimallösung
-	temp.push_back("Fahrbtr");
-	temp.push_back("Starten");
-	temp.push_back("Aus_Ein");
+	/* Write output files */
+	if (writeOutputFile()) {
+		fprintf(stderr, "Ausgabedatei \"ZMnichtoptimiert.tbl\" fehlgeschlagen\n");
+	}
+	else {
+		cout << "Ausgabedatei \"ZMnichtoptimiert.tbl\" erfolgreich geschrieben\n";
+	}
+	if (writeOutputFile(Zustandscodierung)) {
+		fprintf(stderr, "Ausgabedatei \"ZMoptimiert.tbl\" fehlgeschlagen\n");
+	}
+	else {
+		cout << "Ausgabedatei \"ZMoptimiert.tbl\" erfolgreich geschrieben\n";
+	}
+	//smtable::elementlist temp;//testdatei für Schäfers minimallösung
+	//temp.push_back("S0");
+	//temp.push_back("S7");
+	//temp.push_back("S3");
 	//temp.push_back("S1");
 	//temp.push_back("S2");
 	//temp.push_back("S4");
