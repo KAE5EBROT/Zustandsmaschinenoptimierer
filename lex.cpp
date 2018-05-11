7eadf0e13149524e47394eaca5dcb2a5215e46ae
Patrick <patrick.wenninger1@freenet.de>
2018-05-11 17:16:07 +0200

    test optimierung

--- lex.cpp
+++ lex.cpp
@@ -118,17 +118,17 @@ int	CParser::yyparse()
 	Zustandscodierung = optimize(high_priority, mean_priority, lowest_priority);
 
 	writeOutputFile();
-	writeOutputFile(Zustandscodierung);
-	//smtable::elementlist temp;//testdatei für Schäfers minimallösung
-	//temp.push_back("S0");
-	//temp.push_back("S7");
-	//temp.push_back("S3");
+	//writeOutputFile(Zustandscodierung);
+	smtable::elementlist temp;//testdatei für Schäfers minimallösung
+	temp.push_back("Fahrbtr");
+	temp.push_back("Starten");
+	temp.push_back("Aus_Ein");
 	//temp.push_back("S1");
 	//temp.push_back("S2");
 	//temp.push_back("S4");
 	//temp.push_back("S9");
-	//temp.push_back("");
-	//writeOutputFile(temp);
+	temp.push_back("");
+	writeOutputFile(temp);
 	return 0;
 
 }	
