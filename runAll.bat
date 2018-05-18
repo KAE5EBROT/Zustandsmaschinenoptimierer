6213e7aacb62dff55072ca6f287e6e3403f9fd67
David <kaese-david@web.de>
2018-05-18 16:34:20 +0200

    bat

--- runAll.bat
+++ runAll.bat
@@ -2,18 +2,21 @@
 echo Demos:
 echo     1 kleine_automatentabelle.txt
 echo     2 mittlere_automatentabelle.txt
-echo     3 große_automatentabelle.txt
+echo     3 grosse_automatentabelle.txt
 echo     4 riesige_automatentabelle.txt
 set /p inputfc=Choose demo number or enter file:
 if %inputfc%==1 set inputfc=.\kleine_automatentabelle.txt
 if %inputfc%==2 set inputfc=.\mittlere_automatentabelle.txt
-if %inputfc%==3 set inputfc=.\große_automatentabelle.txt
+if %inputfc%==3 set inputfc=.\grosse_automatentabelle.txt
 if %inputfc%==4 set inputfc=.\riesige_automatentabelle.txt
-echo running optimizer for %inputfc%...
-.\Debug\Zustandsmaschinencodierungsoptimierer.exe %inputfc% ::.\kleine_automatentabelle.txt
-echo running minilog...
-cscript runMinilog.vbs
-echo running gate counter...
-"gatter.exe - Verknuepfung.lnk"
-
+if exist %inputfc% (
+	echo running optimizer for %inputfc%...
+	.\Debug\Zustandsmaschinencodierungsoptimierer.exe %inputfc%
+	echo running minilog...
+	cscript runMinilog.vbs
+	echo running gate counter...
+	"gatter.exe - Verknuepfung.lnk"
+) else (
+	echo ERROR: File not found!
+)
 pause
